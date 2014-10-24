09ec62d06fac5664c886dda6d2ef1333737ba5c1
Per-Olov Jernberg <po@spotify.com>
2014-10-24 20:18:49 +0200

    Add sample pixel screen

---
+++ screens/pixel.js
@@ -0,0 +1,14 @@
+var util = require('../lib/util');
+
+var SinglePixelScreen = function() {
+  this.screenFrames = 10;
+  this.g = new util.PixelBuffer();
+}
+
+SinglePixelScreen.prototype.update = function(adapter) {
+  this.g.clear();
+  this.g.setPixel(Math.random() * 64, Math.random() * 16, true);
+  adapter.update(this.g);
+}
+
+exports.Screen = SinglePixelScreen;
